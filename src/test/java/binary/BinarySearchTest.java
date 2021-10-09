fb42856e87962a5122d5b9b64f01f32254de659d
zshnb <a857681664@gmail.com>
2021-10-09 21:20:29 +0800

    init

---
+++ src/test/java/binary/BinarySearchTest.java
@@ -0,0 +1,18 @@
+package binary;
+
+import array.BinarySearch;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class BinarySearchTest {
+    private BinarySearch binarySearch = new BinarySearch();
+
+    @Test
+    public void test() {
+        int[] nums = new int[] {-3, 2, 4, 8, 12, 45, 67};
+        Assertions.assertEquals(4, binarySearch.binarySearch(nums, 12));
+        Assertions.assertEquals(0, binarySearch.binarySearch(nums, -3));
+        Assertions.assertEquals(2, binarySearch.binarySearch(nums, 4));
+        Assertions.assertEquals(-1, binarySearch.binarySearch(nums, 13));
+    }
+}
