21ce77d416d206354d46db78cdfafe0051b76e49
zshnb <a857681664@gmail.com>
2021-11-08 17:15:40 +0800

    feat: BinaryTreeLevelOrderTraversal107

--- src/main/java/leetcode/BinaryTreeLevelOrderTraversal107.java
+++ src/main/java/leetcode/BinaryTreeLevelOrderTraversal107.java
@@ -3,20 +3,20 @@ package leetcode;
 import java.util.*;
 
 /**
- * 利用队列存放当前节点的左右节点，由于题目不是打印，而是返回数据，因此还要记录下节点所在的层级，使得同一层级的节点值放入同一个list
+ * 返回结果为栈，将先遇到的加到后面
  * */
-public class BinaryTreeLevelOrderTraversal102 {
+public class BinaryTreeLevelOrderTraversal107 {
     public List<List<Integer>> levelOrder(TreeNode root) {
         Deque<Pair> queue = new ArrayDeque<>();
-        List<List<Integer>> result = new ArrayList<>();
+        LinkedList<List<Integer>> result = new LinkedList<>();
         if (root == null) return result;
         queue.addLast(new Pair(root, 0));
         while (!queue.isEmpty()) {
             Pair pair = queue.removeFirst();
             if (pair.right == result.size()) {
-                result.add(new ArrayList<>());
+                result.addFirst(new ArrayList<>());
             }
-            result.get(pair.right).add(pair.left.val);
+            result.getFirst().add(pair.left.val);
             if (pair.left.left != null) {
                 queue.addLast(new Pair(pair.left.left, pair.right + 1));
             }
---
+++ src/main/java/leetcode/Pair.java
@@ -0,0 +1,11 @@
+package leetcode;
+
+class Pair {
+    TreeNode left;
+    int right;
+
+    public Pair(TreeNode left, int right) {
+        this.left = left;
+        this.right = right;
+    }
+}
