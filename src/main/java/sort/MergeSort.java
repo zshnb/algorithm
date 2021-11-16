3ce81dd6f030eb5a151af38c915efbe31ea18c7d
zshnb <a857681664@gmail.com>
2021-11-16 21:20:50 +0800

    performance merge sort

--- src/main/java/sort/MergeSort.java
+++ src/main/java/sort/MergeSort.java
@@ -13,19 +13,28 @@ public class MergeSort {
         int mid = (l + r) / 2;
         splitAndSort(nums, l, mid);
         splitAndSort(nums, mid + 1, r);
-        merge(nums, l, mid, r);
+        // 只有当前面区间最后一个元素大于后面区间第一个元素时，数组才是无序状态，才需要进行排序
+        if (nums[mid] > nums[mid + 1]) {
+            merge(nums, l, mid, r);
+        }
     }
 
     /**
-     * 将2个子数组合并成一个有序的子数组，思路跟合并2个有序链表类似，通过2个指针分别指向2个子数组，比较指针处元素大小，让小的指针指向的元素复制到
+     * 将2个子数组合并成一个有序的子数组，思路跟合并2个有序链表类似，通过2个指针i, j分别指向2个子数组，比较指针处元素大小，让小的指针指向的元素复制到
      * 临时数组，然后指针往前移动，需要注意边界问题，左边数组的指针不能超过m，右边数组的指针不能超过r
      * */
     private void merge(int[] nums, int l, int m, int r) {
         int[] tempArray = new int[nums.length];
         System.arraycopy(nums, 0, tempArray, 0, nums.length);
         int i = l, j = m + 1, k = l;
-        while (i <= m && j <= r) {
-            if (tempArray[i] < tempArray[j]) {
+        while (k <= r) {
+            if (i > m) {
+                nums[k] = tempArray[j];
+                j += 1;
+            } else if (j > r) {
+                nums[k] = tempArray[i];
+                i += 1;
+            } else if (tempArray[i] < tempArray[j]) {
                 nums[k] = tempArray[i];
                 i += 1;
             } else {
@@ -33,19 +42,6 @@ public class MergeSort {
                 j += 1;
             }
             k += 1;
-        }
-        if (i == m + 1) {
-            while (j <= r) {
-                nums[k] = tempArray[j];
-                j += 1;
-                k += 1;
-            }
-        } else {
-            while (i <= m) {
-                nums[k] = tempArray[i];
-                i += 1;
-                k += 1;
-            }
         }
     }
 }
