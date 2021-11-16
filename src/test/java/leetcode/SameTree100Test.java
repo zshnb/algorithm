9e31d3aa724826c57f88c2dfb5d5c2868fd0ae0c
zshnb <a857681664@gmail.com>
2021-11-16 20:21:16 +0800

    fix test

--- src/test/java/leetcode/SameTree100Test.java
+++ src/test/java/leetcode/SameTree100Test.java
@@ -11,6 +11,6 @@ public class SameTree100Test {
         TreeNode root1 = new TreeNode(1, new TreeNode(2), new TreeNode(3));
         TreeNode root2 = new TreeNode(1, new TreeNode(2), new TreeNode(3));
         Assertions.assertTrue(instance.isSameTree(root1, root2));
-        Assertions.assertTrue(instance.isSameTree(new TreeNode(1, new TreeNode(2), null), new TreeNode(1, null, new TreeNode(2))));
+        Assertions.assertFalse(instance.isSameTree(new TreeNode(1, new TreeNode(2), null), new TreeNode(1, null, new TreeNode(2))));
     }
 }
