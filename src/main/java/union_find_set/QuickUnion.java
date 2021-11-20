d0a3aeba4dfc53e006b28089b25a0b583321eddc
zshnb <a857681664@gmail.com>
2021-11-20 12:34:09 +0800

    optimize rank

--- src/main/java/union_find_set/QuickUnion.java
+++ src/main/java/union_find_set/QuickUnion.java
@@ -1,24 +1,24 @@
 package union_find_set;
 
 public class QuickUnion {
-    // ids[i]表示i的父下标
-    private int[] ids;
-    // sz[i]表示i的子元素个数
-    private int[] sz;
+    // parent[i]表示i的父下标
+    private int[] parent;
+    // rank[i]表示i的层数
+    private int[] rank;
 
     public QuickUnion(int n) {
-        ids = new int[n];
-        sz = new int[n];
-        for (int i = 0; i < ids.length; i++) {
-            ids[i] = i;
-            sz[i] = 1;
+        parent = new int[n];
+        rank = new int[n];
+        for (int i = 0; i < parent.length; i++) {
+            parent[i] = i;
+            rank[i] = 1;
         }
     }
 
     // 查找给定下标的根节点
     public int find(int index) {
-        while (index != ids[index]) {
-            index = ids[index];
+        while (index != parent[index]) {
+            index = parent[index];
         }
         return index;
     }
@@ -27,17 +27,18 @@ public class QuickUnion {
         return find(p) == find(q);
     }
 
-    // 合并2个节点的时候将子元素少的节点指向子元素大的节点，优化查询
+    // 合并2个节点的时候将层数低的节点指向层数高的节点，优化查询
     public void union(int p, int q) {
         int pRoot = find(p);
         int qRoot = find(q);
         if (qRoot != pRoot) {
-            if (sz[pRoot] < sz[qRoot]) {
-                ids[pRoot] = qRoot;
-                sz[qRoot] += sz[pRoot];
+            if (rank[pRoot] < rank[qRoot]) {
+                parent[pRoot] = qRoot;
+            } else if (rank[pRoot] > rank[qRoot]){
+                parent[qRoot] = pRoot;
             } else {
-                ids[qRoot] = pRoot;
-                sz[pRoot] += sz[qRoot];
+                parent[pRoot] = qRoot;
+                rank[qRoot] += 1;
             }
         }
     }
