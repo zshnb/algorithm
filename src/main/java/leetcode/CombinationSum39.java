e67f2f8d1461bfd91a956190da8df8e46fe25943
zshnb <a857681664@gmail.com>
2021-11-23 22:29:51 +0800

    feat: CombinationSum39

---
+++ src/main/java/leetcode/CombinationSum39.java
@@ -0,0 +1,32 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 思路1：先排序，然后回溯，遇到大于剩下数时剪枝，当剩下的数为0时将路径加入结果
+ * */
+public class CombinationSum39 {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> path = new ArrayList<>();
+        recursive(result, path, target, candidates, 0);
+        return result;
+    }
+
+    private void recursive(List<List<Integer>> result, List<Integer> path, int target, int[] candidates, int begin) {
+        if (target == 0) {
+            result.add(new ArrayList<>(path));
+            return;
+        }
+
+        for (int i = begin; i < candidates.length; i++) {
+            if (target - candidates[i] < 0) {
+                return;
+            }
+            path.add(candidates[i]);
+            recursive(result, path, target - candidates[i], candidates, i + 1);
+            path.remove(path.size() - 1);
+        }
+    }
+}
