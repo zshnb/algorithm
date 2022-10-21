1fdb338309050f49159c904265bdac348b02151d
zshnb <a857681664@gmail.com>
2022-10-21 21:43:04 +0800

    feat: OnlineStockSpan901

--- src/main/java/leetcode/OnlineStockSpan901.java
+++ src/main/java/leetcode/OnlineStockSpan901.java
@@ -1,15 +1,21 @@
 package leetcode;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Deque;
 import java.util.List;
 
 /**
  * 思路1：暴力遍历
+ * 思路1：使用栈来存放价格和其对应最大连续天数，当新价格入栈时，判断栈顶元素价格，如果大于新价格，新价格入栈，天数为1，否则栈顶元素出栈，对应天数累加到
+ * 当前价格天数，直到栈顶元素大于新价格，此时新价格入栈，同时设置天数
  * */
 public class OnlineStockSpan901 {
     private List<Integer> prices;
+    private Deque<Stock> stocks;
     public OnlineStockSpan901() {
         prices = new ArrayList<>();
+        stocks = new ArrayDeque<>();
     }
 
     public int next(int price) {
@@ -24,5 +30,32 @@ public class OnlineStockSpan901 {
             }
         }
         return result;
+    }
+
+    public int next2(int price) {
+        if (stocks.isEmpty()) {
+            stocks.addFirst(new Stock(price, 1));
+            return 1;
+        }
+        if (stocks.peekFirst().price > price) {
+            stocks.addFirst(new Stock(price, 1));
+            return 1;
+        }
+        int day = 1;
+        while (!stocks.isEmpty() && stocks.peekFirst().price <= price) {
+            day += stocks.pollFirst().day;
+        }
+        stocks.addFirst(new Stock(price, day));
+        return day;
+    }
+
+    static class Stock {
+        int price;
+        int day;
+
+        public Stock(int price, int day) {
+            this.price = price;
+            this.day = day;
+        }
     }
 }
--- src/test/java/leetcode/OnlineStockSpan901Test.java
+++ src/test/java/leetcode/OnlineStockSpan901Test.java
@@ -15,4 +15,16 @@ public class OnlineStockSpan901Test {
         Assertions.assertEquals(4, obj.next(75));
         Assertions.assertEquals(6, obj.next(85));
     }
+
+    @Test
+    public void successful2() {
+        OnlineStockSpan901 obj = new OnlineStockSpan901();
+        Assertions.assertEquals(1, obj.next2(100));
+        Assertions.assertEquals(1, obj.next2(80));
+        Assertions.assertEquals(1, obj.next2(60));
+        Assertions.assertEquals(2, obj.next2(70));
+        Assertions.assertEquals(1, obj.next2(60));
+        Assertions.assertEquals(4, obj.next2(75));
+        Assertions.assertEquals(6, obj.next2(85));
+    }
 }
